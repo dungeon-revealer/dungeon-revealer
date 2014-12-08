689f6f48ca7771fa2378474a5e09abe5b774cbc8
apclary <apclary@gmail.com>
2014-12-08 03:34:19 -0600

    Fixed issue with tall maps not showing

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -6,7 +6,13 @@ socket.on('map update', function(msg) {
   console.log(msg);
   if (msg && msg.imageData) {
     console.log('updating the map');
-    document.body.style.backgroundImage = 'url(' + msg.imageData + ')';
-    document.body.style.backgroundSize = 'cover';
+    $('#map').remove();
+    var map = new Image();
+    map.id = 'map';
+    map.style.width = '100%';
+    map.src = msg.imageData;
+    document.getElementById('map-container').appendChild(map);
+    //document.body.style.backgroundImage = 'url(' + msg.imageData + ')';
+    //document.body.style.backgroundSize = 'cover';
   }
 });
