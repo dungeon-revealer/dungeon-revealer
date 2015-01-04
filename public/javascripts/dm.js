abf6116305e7f013fcd216d14e501d50f8268c14
apclary <apclary@gmail.com>
2015-01-04 03:30:29 -0600

    Minor stuff

--- public/javascripts/dm.js
+++ public/javascripts/dm.js
@@ -175,53 +175,53 @@ function setUp() {
   mapCanvas = document.getElementById('mapCanvas');
   mapContext = mapCanvas.getContext('2d');
 
-// Need to move this somewhere else
-brush = (function(context, settings) {
-  var brushTypes = ["clear", "fog"],
-      currentBrushType = brushTypes[0],
-      currentPattern = null,
-      setBrushType = function() {
-        console.error("Doesn't exist yet");
-      },
-      toggle = function() {
-        if (currentBrushType === brushTypes[0]) {
-          console.log("shroud brush set");
-          currentBrushType = brushTypes[1];
-        } else if (currentBrushType === brushTypes[1]) {
+  // Need to move this somewhere else
+  brush = (function(context, settings) {
+    var brushTypes = ["clear", "fog"],
+        currentBrushType = brushTypes[0],
+        currentPattern = null,
+        setBrushType = function() {
+          console.error("Doesn't exist yet");
+        },
+        toggle = function() {
+          if (currentBrushType === brushTypes[0]) {
+            console.log("shroud brush set");
+            currentBrushType = brushTypes[1];
+          } else if (currentBrushType === brushTypes[1]) {
 
-          console.log("clear brush set");
-          currentBrushType = brushTypes[0];
-        } else {
-          console.log("nothing: ");
-          console.log(currentBrushType);
+            console.log("clear brush set");
+            currentBrushType = brushTypes[0];
+          } else {
+            console.log("nothing: ");
+            console.log(currentBrushType);
+          }
+          context.strokeStyle = getCurrent();
+        },
+        getPattern = function(brushType) {
+          if (brushType === brushTypes[0]) {
+      		 context.globalCompositeOperation = 'destination-out';
+            //return dmContext.createPattern(getImageCanvas(), 'repeat');
+            return 'rgba('+settings.fogRGB+','+settings.fogOpacity+')';
+          } else if (brushType === brushTypes[1]) {
+            context.globalCompositeOperation = 'source-over';
+            return 'rgba('+settings.fogRGB+','+settings.fogOpacity+')';
+            //return dmContext.createPattern(getShadowImageCanvas(), 'repeat');
+          }
+
+        },
+        getCurrent = function() {
+          return getPattern(currentBrushType);
         }
-        context.strokeStyle = getCurrent();
-      },
-      getPattern = function(brushType) {
-        if (brushType === brushTypes[0]) {
-    		 context.globalCompositeOperation = 'destination-out';
-          //return dmContext.createPattern(getImageCanvas(), 'repeat');
-          return 'rgba('+settings.fogRGB+','+settings.fogOpacity+')';
-        } else if (brushType === brushTypes[1]) {
-          context.globalCompositeOperation = 'source-over';
-          return 'rgba('+settings.fogRGB+','+settings.fogOpacity+')';
-          //return dmContext.createPattern(getShadowImageCanvas(), 'repeat');
-        }
 
-      },
-      getCurrent = function() {
-        return getPattern(currentBrushType);
-      }
-
-  return {
-    brushTypes: brushTypes,
-    currentBrushType: currentBrushType,
-    setBrushType: setBrushType,
-    toggle: toggle,
-    getCurrent: getCurrent,
-    getPattern: getPattern
-  }
-})(dmContext, settings);
+    return {
+      brushTypes: brushTypes,
+      currentBrushType: currentBrushType,
+      setBrushType: setBrushType,
+      toggle: toggle,
+      getCurrent: getCurrent,
+      getPattern: getPattern
+    }
+  })(dmContext, settings);
 
 
 
