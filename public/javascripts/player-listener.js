25f20914ccf4eb41d5d90d2bae56c4fc56b5cb8b
Hugo Hromic <hhromic@gmail.com>
2018-01-13 17:03:03 +0000

    Revert "fit map to full window in player view"
    This reverts commit 7b11b83b76a1e0e230bd25f5d578bc8011873559.

--- public/javascripts/player-listener.js
+++ public/javascripts/player-listener.js
@@ -11,6 +11,7 @@ define(['settings', 'jquery', 'io'], function (settings, jquery, io) {
             $('.map').addClass('oldest').css('z-index', '30');
             var oldMap = document.querySelector('.map');
             var newMap = new Image();
+            newMap.style.width = '100%';
             newMap.style.zIndex = 20;
             newMap.src = msg.imageData;
             newMap.className = "map";
