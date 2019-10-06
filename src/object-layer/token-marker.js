cd04adb0f681ba7dd755244c299020250fcb697e
n1ru4l <laurinquast@googlemail.com>
2019-10-06 18:37:46 +0200

    feat: animate token updates for players.

--- src/object-layer/token-marker.js
+++ src/object-layer/token-marker.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { darken } from "polished";
+import { useSpring, animated, interpolate } from "react-spring";
 
 export const TokenMarker = React.memo(
   React.forwardRef(
@@ -16,6 +17,7 @@ export const TokenMarker = React.memo(
         onMouseDown,
         onContextMenu,
         ratio = 1,
+        isAnimated = true,
         ...props
       },
       ref
@@ -23,8 +25,17 @@ export const TokenMarker = React.memo(
       const gRef = useRef(null);
       const circleRef = useRef(null);
       const textRef = useRef(null);
-
       const ratioRef = useRef(ratio);
+
+      const {
+        x: animatedX,
+        y: animatedY,
+        radius: animatedRadius,
+        color: animatedColor
+      } = useSpring({
+        to: { x, y, radius, color },
+        immediate: !isAnimated
+      });
 
       useEffect(() => {
         ratioRef.current = ratio;
@@ -48,36 +59,41 @@ export const TokenMarker = React.memo(
         }
       });
 
+      const realRadius = animatedRadius.interpolate(r => r * ratio);
+
       return (
-        <g
+        <animated.g
           ref={gRef}
-          transform={`translate(${x * ratio}, ${y * ratio})`}
+          transform={interpolate(
+            [animatedX, animatedY],
+            (x, y) => `translate(${x * ratio}, ${y * ratio})`
+          )}
           opacity={isVisibleForPlayers ? 1 : 0.7}
           {...props}
         >
-          <circle
+          <animated.circle
             ref={circleRef}
-            r={radius * ratio}
-            strokeWidth={radius * ratio * 0.05}
-            stroke={darken(0.1, color)}
-            fill={color}
+            r={realRadius}
+            strokeWidth={realRadius.interpolate(val => val * 0.05)}
+            stroke={animatedColor.interpolate(value => darken(0.1, value))}
+            fill={animatedColor}
             opacity="1"
             onClick={onClick}
             onMouseDown={onMouseDown}
             onContextMenu={onContextMenu}
           />
 
-          <text
+          <animated.text
             ref={textRef}
             pointerEvents="none"
             textAnchor="middle"
             stroke="black"
-            fontSize={radius * ratio}
+            fontSize={realRadius}
             dy=".3em"
           >
             {label}
-          </text>
-        </g>
+          </animated.text>
+        </animated.g>
       );
     }
   )
