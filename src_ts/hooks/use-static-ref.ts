6cf8d3c5dbc3c608dbc386cd74f465e7238482db
n1ru4l <laurinquast@googlemail.com>
2020-02-21 16:04:36 +0100

    convert use-static-ref hook

--- src_ts/hooks/use-static-ref.ts
+++ src_ts/hooks/use-static-ref.ts
@@ -1,11 +1,12 @@
 import { useRef } from "react";
 
 // creates a ref that must be mutated but cannot be reassigned.
-export const useStaticRef = create => {
-  const ref = useRef();
+export const useStaticRef = <TRefType extends any>(
+  create: () => TRefType
+): TRefType => {
+  const ref = useRef<TRefType>();
   if (!ref.current) {
     ref.current = create();
   }
-
   return ref.current;
 };
