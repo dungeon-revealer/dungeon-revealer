6cd8490dc2c67fb08778d60d0322e7459d69d8af
Laurin Quast <laurinquast@googlemail.com>
2020-03-21 10:53:40 +0100

    fix(player-area): show splash screen when no map is shared with the players (#296)

--- src/splash-screen.js
+++ src/splash-screen.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BackgroundImageContainer } from "./background-image-container";
 import { BrandLogoText } from "./brand-logo-text";
+import styled from "@emotion/styled/macro";
 
-export const LoadingScreen = () => {
+const SplashScreenText = styled.div`
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const SplashScreen = ({ text = null }) => {
   return (
     <BackgroundImageContainer>
       <div>
         <BrandLogoText />
-        <div>Loading....</div>
+        <SplashScreenText>{text}</SplashScreenText>
       </div>
     </BackgroundImageContainer>
   );
