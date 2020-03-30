25e72c173958aaea2176a533060a7338597561b5
Laurin Quast <laurinquast@googlemail.com>
2020-03-30 19:42:50 +0200

    fix(useIsKeyPressed): reset value when blurring the window. (#320)

--- src/hooks/use-is-key-pressed.js
+++ src/hooks/use-is-key-pressed.js
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from "react";
 export const useIsKeyPressed = (key) => {
   const [isPressed, setIsPressed] = useState(false);
   const keyRef = useRef(key);
+
   useEffect(() => {
     const onKeyDown = (ev) => {
       if (ev.key === keyRef.current) setIsPressed(true);
@@ -10,12 +11,18 @@ export const useIsKeyPressed = (key) => {
     const onKeyUp = (ev) => {
       if (ev.key === keyRef.current) setIsPressed(false);
     };
+    const onBlur = () => {
+      setIsPressed(false);
+    };
+
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("blur", onBlur);
 
     return () => {
       window.removeEventListener("keyup", onKeyUp);
       window.removeEventListener("keydown", onKeyDown);
+      window.removeEventListener("blur", onBlur);
     };
   }, []);
 
