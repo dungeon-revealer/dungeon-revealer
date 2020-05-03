732df90e6997de4020af4d16b9e255fb62a4ac8a
n1ru4l <laurinquast@googlemail.com>
2020-05-03 22:48:56 +0200

    wip - chat message history (send again)

--- src/chat/chat-message.tsx
+++ src/chat/chat-message.tsx
@@ -4,34 +4,52 @@ import graphql from "babel-plugin-relay/macro";
 import { chatMessage_message } from "./__generated__/chatMessage_message.graphql";
 import styled from "@emotion/styled";
 
-function format_two_digits(n: number) {
+function formatTwoDigits(n: number) {
   return n < 10 ? "0" + n : n;
 }
 
 const formatTime = (t: string) => {
   const d = new Date(t);
-  const hours = format_two_digits(d.getHours());
-  const minutes = format_two_digits(d.getMinutes());
+  const hours = formatTwoDigits(d.getHours());
+  const minutes = formatTwoDigits(d.getMinutes());
   return hours + ":" + minutes;
 };
 
 const Time = styled.span`
   line-height: inherit;
   font-size: 10px;
-  padding-top: 5px;
+  padding-top: 4px;
   padding-right: 4px;
+`;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Column = styled.div`
+  padding-bottom: 4px;
+  padding-top: 4px;
+`;
+
+const AuthorName = styled.span`
+  font-weight: bold;
 `;
 
 const ChatMessageRenderer: React.FC<{
   message: chatMessage_message;
 }> = React.memo(({ message }) => {
   return (
-    <div style={{ display: "flex", paddingTop: 4, paddingBottom: 4 }}>
-      <Time>{formatTime(message.createdAt)}</Time>
-      <span style={{ fontWeight: "bold" }}>{message.authorName}</span>
-      {": "}
-      {message.rawContent}
-    </div>
+    <Container>
+      <Column>
+        <Time>{formatTime(message.createdAt)}</Time>
+      </Column>
+      <Column>
+        <AuthorName style={{ fontWeight: "bold" }}>
+          {message.authorName}{" "}
+        </AuthorName>
+        {message.rawContent}
+      </Column>
+    </Container>
   );
 });
 
--- src/chat/chat-textarea.tsx
+++ src/chat/chat-textarea.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import styled from "@emotion/styled/macro";
+import createPersistedState from "use-persisted-state";
 import { useMessageAddMutation } from "./message-add-mutation";
 
 const Container = styled.form`
   display: flex;
-  margin-top: 4px;
+  margin-top: 8px;
 `;
 
 const TextArea = styled.textarea`
@@ -14,8 +15,35 @@ const TextArea = styled.textarea`
   font-size: inherit;
 `;
 
+const useRawChatHistory = createPersistedState("chat.history");
+
+const useChatHistory = (size = 10): [string[], (text: string) => void] => {
+  const [data, setData] = useRawChatHistory(() => [] as unknown);
+
+  const pushValue = React.useCallback(
+    (text) => {
+      setData((data: unknown) => {
+        const newData = Array.isArray(data) ? [text, ...data] : [text];
+        if (newData.length > size) {
+          newData.slice(0, newData.length - size);
+        }
+        return newData;
+      });
+    },
+    [size]
+  );
+
+  return React.useMemo(() => {
+    const history = Array.isArray(data) ? data : [];
+    return [history, pushValue];
+  }, [data, pushValue]);
+};
+
 export const ChatTextArea: React.FC<{}> = () => {
   const [value, setValue] = React.useState("");
+  const [chatHistory, pushToHistory] = useChatHistory();
+  const offset = React.useRef(-1);
+
   const messageAdd = useMessageAddMutation();
 
   const onChange = React.useCallback(
@@ -26,26 +54,50 @@ export const ChatTextArea: React.FC<{}> = () => {
   );
 
   const onSubmit = React.useCallback(() => {
+    if (value.trim() === "") return;
     messageAdd({
       rawContent: value,
       authorName: "John Lenon",
     });
     setValue("");
+    pushToHistory(value);
+    offset.current = -1;
   }, [messageAdd, value]);
 
   const onKeyPress = React.useCallback(
     (ev: React.KeyboardEvent) => {
-      if (ev.key === "Enter") {
+      if (ev.key === "Enter" && ev.shiftKey === false) {
         onSubmit();
         ev.preventDefault();
       }
     },
-    [onSubmit]
+    [onSubmit, chatHistory, value]
+  );
+
+  const onKeyDown = React.useCallback(
+    (ev: React.KeyboardEvent) => {
+      if (ev.key === "ArrowUp" && ev.shiftKey === true) {
+        offset.current = Math.min(offset.current + 1, chatHistory.length - 1);
+        setValue((value) => chatHistory[offset.current] || value);
+      } else if (ev.key === "ArrowDown" && ev.shiftKey === true) {
+        offset.current = Math.max(offset.current - 1, -1);
+        if (offset.current === -1) {
+          setValue("");
+        } else {
+          setValue((value) => chatHistory[offset.current] || value);
+        }
+      }
+    },
+    [value]
   );
 
   return (
-    <Container onSubmit={onSubmit} onKeyPress={onKeyPress}>
-      <TextArea value={value} onChange={onChange} rows={4} />
+    <Container
+      onSubmit={onSubmit}
+      onKeyPress={onKeyPress}
+      onKeyDown={onKeyDown}
+    >
+      <TextArea value={value} onChange={onChange} rows={3} />
     </Container>
   );
 };
