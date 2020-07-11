765ee336a9b0f2414742925c7b6ca07b0cb2af2b
Laurin Quast <laurinquast@googlemail.com>
2020-07-11 12:28:36 +0200

    fix: search styles (#565)
    closes https://github.com/dungeon-revealer/dungeon-revealer/issues/564

--- src/global-styles.tsx
+++ src/global-styles.tsx
@@ -35,6 +35,12 @@ export const globalStyles = css`
   #root {
     height: 100%;
     overflow: hidden;
+    /* Prevent content dragging on safari */
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
   }
 
   input[type="range"] {
