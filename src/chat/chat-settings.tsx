a25af6a2771970cb7fca906f08efa8477a2f9797
n1ru4l <laurinquast@googlemail.com>
2020-07-12 20:25:48 +0200

    feat: configure sound effects

--- src/chat/chat-settings.tsx
+++ src/chat/chat-settings.tsx
@@ -4,10 +4,12 @@ import graphql from "babel-plugin-relay/macro";
 import { chatSettings_data } from "./__generated__/chatSettings_data.graphql";
 import { Input } from "../input";
 import * as Button from "../button";
+import * as HorizontalNavigation from "../horizontal-navigation";
 import { useResetState } from "../hooks/use-reset-state";
 import { useChangeNameMutation } from "./change-name-mutation";
 import styled from "@emotion/styled/macro";
 import * as Icon from "../feather-icons";
+import { useSoundSettings } from "../sound-settings";
 
 const LabelText = styled.div`
   text-transform: uppercase;
@@ -22,6 +24,8 @@ const ChatSettingsRenderer: React.FC<{ data: chatSettings_data }> = ({
 }) => {
   const [name, setValue] = useResetState(data.name, [data.name]);
   const changeName = useChangeNameMutation();
+  const soundSettings = useSoundSettings();
+
   return (
     <>
       <label>
@@ -41,6 +45,33 @@ const ChatSettingsRenderer: React.FC<{ data: chatSettings_data }> = ({
         <Icon.CheckIcon height={16} width={16} />
         <span>Save</span>
       </Button.Primary>
+      <div style={{ height: 24 }} />
+      <label>
+        <LabelText>Chat Sound</LabelText>
+        <HorizontalNavigation.Group>
+          <HorizontalNavigation.Button
+            small
+            isActive={soundSettings.value === "all"}
+            onClick={() => soundSettings.setValue("all")}
+          >
+            All
+          </HorizontalNavigation.Button>
+          <HorizontalNavigation.Button
+            small
+            isActive={soundSettings.value === "dice-only"}
+            onClick={() => soundSettings.setValue("dice-only")}
+          >
+            Dice only
+          </HorizontalNavigation.Button>
+          <HorizontalNavigation.Button
+            small
+            isActive={soundSettings.value === "none"}
+            onClick={() => soundSettings.setValue("none")}
+          >
+            None
+          </HorizontalNavigation.Button>
+        </HorizontalNavigation.Group>
+      </label>
     </>
   );
 };
