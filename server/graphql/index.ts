f6d76e16448a9950872f5014e8ff72017bbbaf03
n1ru4l <laurinquast@googlemail.com>
2020-09-14 16:01:26 +0200

    improve the node field typings

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -31,13 +31,23 @@ import { pipe } from "fp-ts/lib/pipeable";
 import * as E from "fp-ts/lib/Either";
 import * as RT from "fp-ts/lib/ReaderTask";
 import { GraphQLLiveDirective } from "@n1ru4l/graphql-live-query";
-import { Interface } from "gqtx/dist/types";
+
+type ReaderDependencyType<T> = T extends RT.ReaderTask<infer Deps, any>
+  ? Deps
+  : never;
+
+// for making the TypeScript compiler happy we have to cast all possible return types
+// if you know a better way let me know :)
+// prettier-ignore
+type ReaderTaskType =
+  ReaderDependencyType<ReturnType<typeof NotesModule.resolveNote>>
+  & ReaderDependencyType<ReturnType<typeof MapModule.resolveMapByDatabaseId>>
+;
+
+type NodeReaderTaskType = RT.ReaderTask<ReaderTaskType, { id: string }>;
 
 const nodeField = t.field("node", {
-  type: RelaySpecModule.GraphQLNodeInterface as Interface<
-    GraphQLContextType,
-    any
-  >,
+  type: RelaySpecModule.GraphQLNodeInterface,
   args: {
     id: t.arg(t.NonNullInput(t.ID)),
   },
@@ -46,18 +56,28 @@ const nodeField = t.field("node", {
       pipe(
         RelaySpecModule.decodeId(args.id),
         E.fold(
-          () => RT.of(null),
+          (_err) => {
+            // TODO: better error handling
+            // the best thing would be to have a "resolver" abstraction that takes either
+            // - Either<unknown, Error | Errors>
+            // - TaskEither<unknown, Error | Errors>
+            // - ReaderTaskEither<unknown, Error | Errors>
+            // Pretty prints those errors to the console and then returns null/throws or throws an "Unexpected Server" error,
+            // so the error is not propagated to the outside world
+            throw new Error("Invalid id provided.");
+          },
           ([version, type, id]) => {
             if (version !== RelaySpecModule.API_VERSION) {
               return RT.of(null);
             }
-            // We have to cast to any here otherwise the TypeScript compiler complains :(
-            // If you are digging the code and know better please fix it :)
+
             switch (type) {
               case NotesModule.NOTE_URI:
-                return NotesModule.resolveNote(id) as any;
+                return NotesModule.resolveNote(id) as NodeReaderTaskType;
               case MapModule.MAP_URI:
-                return MapModule.resolveMapByDatabaseId({ id }) as any;
+                return MapModule.resolveMapByDatabaseId({
+                  id,
+                }) as NodeReaderTaskType;
             }
 
             return RT.of(null);
