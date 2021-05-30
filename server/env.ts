48e7b82efdb464b65fc89988382e2903d00c6c11
Laurin Quast <laurinquast@googlemail.com>
2021-05-30 11:10:51 +0200

    refactor: convert env.js file to typescript

---
+++ server/env.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { getDefaultDataDirectory } from "./util";
+
+export const getEnv = (env: NodeJS.ProcessEnv) => {
+  const PUBLIC_PATH = path.resolve(__dirname, "..", "build");
+  const PUBLIC_URL = env.PUBLIC_URL || "";
+  const PORT = env.PORT || 3000;
+  const HOST = env.HOST || "0.0.0.0";
+  const PC_PASSWORD = env.PC_PASSWORD || null;
+  const DM_PASSWORD = env.DM_PASSWORD || null;
+  const DATA_DIRECTORY = env.DATA_DIRECTORY || getDefaultDataDirectory();
+
+  return {
+    DATA_DIRECTORY,
+    PUBLIC_PATH,
+    PUBLIC_URL,
+    PORT,
+    HOST,
+    PC_PASSWORD,
+    DM_PASSWORD,
+  };
+};
