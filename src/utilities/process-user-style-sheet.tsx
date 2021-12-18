536e5b0be50069e30d0cccdecf963df55deba776
Laurin Quast <laurinquast@googlemail.com>
2021-12-18 14:13:27 +0100

    update list of allowed css properties

--- src/utilities/process-user-style-sheet.tsx
+++ src/utilities/process-user-style-sheet.tsx
@@ -17,9 +17,14 @@ const isXSSValue = (value: string) => regexJavascriptUrl.test(value);
 
 type CSSRule = boolean | ((value: string) => boolean);
 
-const allowList: Record<string, CSSRule | undefined> = {
-  color: true,
+type CSSConfig = Record<string, CSSRule | undefined>;
+
+const cssBasics: CSSConfig = {
+  display: true,
   "background-color": true,
+};
+
+const cssBox: CSSConfig = {
   padding: true,
   "padding-left": true,
   "padding-right": true,
@@ -30,10 +35,55 @@ const allowList: Record<string, CSSRule | undefined> = {
   "margin-right": true,
   "margin-bottom": true,
   "margin-top": true,
+  height: true,
+  width: true,
+  "max-width": true,
+  "min-width": true,
+  "max-height": true,
+  "min-height": true,
+  border: true,
+  left: true,
+  right: true,
+  float: true,
+};
+
+const cssFont: CSSConfig = {
+  color: true,
+  "font-size": true,
+  "font-family": true,
+  "text-align": true,
+  "line-height": true,
+  "font-style": true,
+  "font-weight": true,
+  "text-transform": true,
+  "text-decoration": true,
+  "text-shadow": true,
+};
+
+const cssFlex: CSSConfig = {
+  flex: true,
+  "flex-basis": true,
+  "flex-direction": true,
+  "flex-flow": true,
+  "flex-grow": true,
+  "flex-shrink": true,
+  "flex-wrap": true,
+  order: true,
+  "justify-content": true,
+  "align-items": true,
+  "align-self": true,
+  "align-content": true,
+};
+
+const allowList: CSSConfig = {
+  ...cssBasics,
+  ...cssFont,
+  ...cssBox,
+  ...cssFlex,
 };
 
 const applyPropertyAllowListPLugin = (): Plugin => ({
-  postcssPlugin: "fapply-property-allow-list",
+  postcssPlugin: "apply-property-allow-list",
   Declaration: (declarationNode, helpers) => {
     const rule = allowList[declarationNode.prop];
 
